5185b8f02db864578669ec1200172565ac1776bd
0xRTekk <remi.sulpice@oclock.io>
2024-01-08 21:52:19 +0100

    Fix deps

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from "react";
-// import Tilt from "react-tilt";
+// import { Tilt } from "react-tilt";
 import { motion, useInView } from "framer-motion";
 
 import { styles } from "../styles";
@@ -55,7 +55,7 @@ const SkillPanel = ({ category, icon, gradient, technologies, carouselIndex, set
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              stroke-width="1.5"
+              strokeWidth="1.5"
               stroke={`${carouselIndex <= 0 ? 'grey' : 'currentColor'}`}
               className={`h-10 w-10 ${carouselIndex <= 0 ? '' : 'cursor-pointer'}`}
               onClick={() => {
@@ -65,8 +65,8 @@ const SkillPanel = ({ category, icon, gradient, technologies, carouselIndex, set
               }}
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
             
@@ -77,7 +77,7 @@ const SkillPanel = ({ category, icon, gradient, technologies, carouselIndex, set
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              stroke-width="1.5"
+              strokeWidth="1.5"
               stroke={`${carouselIndex >= 3 ? 'grey' : 'currentColor'}`}
               className={`h-10 w-10 ${carouselIndex >= 3 ? '' : 'cursor-pointer'}`}
               onClick={() => {
@@ -87,8 +87,8 @@ const SkillPanel = ({ category, icon, gradient, technologies, carouselIndex, set
               }}
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
           </div>

e7720460f6216c6ef4e3bd272045633ae0eba4be
0xRTekk <remi.sulpice@oclock.io>
2024-01-09 01:07:05 +0100

    Global rework

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import Home from './pages/Home';
-import './App.css';
+import { BrowserRouter } from "react-router-dom";
 
-function App() {
+import { Skills, Contact, Experiences, Hero, Navbar, Projects, StarsCanvas } from "./components";
 
+const App = () => {
   return (
-    <Home />
-  )
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
+        </div>
+        <Skills />
+        <Projects />
+        <Experiences />
+        <div className='relative z-0'>
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {NextUIProvider} from '@nextui-org/react';
-import App from './App.jsx';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <NextUIProvider>
-      <App />
-    </NextUIProvider>
-  </React.StrictMode>,
-)
+    <App />
+  </React.StrictMode>
+);

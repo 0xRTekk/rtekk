10d472c3757afaf5bb71aacadfbd25662eb7dd16
0xRTekk <remi.sulpice@oclock.io>
2024-07-27 21:49:22 +0200

    feat: About section

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,87 +1,132 @@
-import React, { useLayoutEffect, useRef } from "react";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import gsap from "gsap";
+import React, { useState, useRef } from "react";
+import { motion, useScroll, useTransform } from 'framer-motion';
 import Image from 'next/image';
 import styles from "./style.module.css";
 
-const phrases = [
-  "Fullstack developer",
-  "Frontend and Web design enthusiast",
-  "Deep lover of learning and teaching",
-  "Blockchain technology advocate & builder",
-  "Currently based in France",
-];
 
 export default function Index() {
+  const paragraph = "Fullstack developer. Frontend and Web design enthusiast. Deep lover of learning and teaching. Blockchain technology advocate & builder. Currently based in France.";
   return (
     <div className={styles.about}>
       <h2>About</h2>
       <h3>Get to know me</h3>
-      <p>Fullstack developer</p>
-      <p>Frontend and Web design enthusiast</p>
-      <p>Deep lover of learning and teaching</p>
-      <p>Blockchain technology advocate & builder</p>
-      <p>Currently based in France</p>
-      {/* {phrases.map((phrase, index) => {
-        return <AnimatedText key={index}>{phrase}</AnimatedText>;
-      })} */}
+      <div className={styles.aboutContainer}>
+        <Paragraph paragraph={paragraph} />
+        <div className={styles.aboutImage}>
+          <Image
+            src={'/images/hero_image.jfif'}
+            alt="About picture"
+            fill={true} 
+            priority={true}
+          />
+        </div>
+      </div>
       <Socials />
     </div>
   );
 }
 
-function AnimatedText({ children }) {
-  const text = useRef(null);
 
-  useLayoutEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
-    gsap.from(text.current, {
-      scrollTrigger: {
-        trigger: text.current,
-        scrub: true,
-        start: "-=350px",
-        end: "+=50px",
-        markers: true,
-      },
-      opacity: 0,
-      left: "-200px",
-      ease: "power3.inOut",
-    });
-  }, []);
+function Paragraph({paragraph}) {
 
-  return <p ref={text}>{children}</p>;
+  const container = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start 0.9", "start 0.25"]
+  })
+
+  const words = paragraph.split(" ")
+  return (
+    <p 
+      ref={container}         
+      className={styles.paragraph}
+    >
+    {
+      words.map( (word, i) => {
+        const start = i / words.length
+        const end = start + (1 / words.length)
+        return <Word key={i} progress={scrollYProgress} range={[start, end]}>{word}</Word>
+      })
+    }
+    </p>
+  )
+}
+
+const Word = ({children, progress, range}) => {
+  const opacity = useTransform(progress, range, [0, 1])
+  return <span className={styles.word}>
+    <span className={styles.shadow}>{children}</span>
+    <motion.span style={{opacity: opacity}}>{children}</motion.span>
+  </span>
 }
 
 function Socials() {
   return (
     <div className={styles.socials}>
-      <a href="https://github.com/0xRTekk/">
-        <Image
-          src="/images/github.png"
-          alt="github icon"
-          width={50}
-          height={50}
-          // sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-        />
-      </a>
-      <a href="https://www.linkedin.com/in/remi-sulpice">
-        <Image
-          src="/images/linkedin.svg"
-          alt="linkedin icon"
-          width={40}
-          height={40}
-          // sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-        />
-      </a>
-      <a href="mailto:remi.sulpice@proton.me">
-        <Image
-          src="/images/mail.svg"
-          alt="mail icon"
-          width={40}
-          height={40}
-          // sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-        />
-      </a>
+      <Magnetic>
+        <a href="https://github.com/0xRTekk/">
+          <Image
+            src="/images/github2.svg"
+            alt="github icon"
+            width={50}
+            height={50}
+            // sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          />
+        </a>
+      </Magnetic>
+      <Magnetic>
+        <a href="https://www.linkedin.com/in/remi-sulpice">
+          <Image
+            src="/images/linkedin.svg"
+            alt="linkedin icon"
+            width={40}
+            height={40}
+            // sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          />
+        </a>
+      </Magnetic>
+      <Magnetic>
+        <a href="mailto:remi.sulpice@proton.me">
+          <Image
+            src="/images/mail.svg"
+            alt="mail icon"
+            width={40}
+            height={40}
+            // sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          />
+        </a>
+      </Magnetic>
     </div>
   );
 };
+
+function Magnetic({children}) {
+    const ref = useRef(null);
+    const [position, setPosition] = useState({x:0,y:0});
+
+    const handleMouse = (e) => {
+        const { clientX, clientY } = e;
+        const {height, width, left, top} = ref.current.getBoundingClientRect();
+        const middleX = clientX - (left + width/2)
+        const middleY = clientY - (top + height/2)
+        setPosition({x: middleX, y: middleY})
+    }
+
+    const reset = () => {
+        setPosition({x:0, y:0})
+    }
+
+    const { x, y } = position;
+    return (
+        <motion.div
+            style={{position: "relative"}}
+            ref={ref}
+            onMouseMove={handleMouse}
+            onMouseLeave={reset}
+            animate={{x, y}}
+            transition={{type: "spring", stiffness: 150, damping: 15, mass: 0.1}}
+        >
+            {children}
+        </motion.div>
+    )
+}

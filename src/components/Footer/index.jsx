8a74d925104d4e7694d13d7ed54afe917206b419
0xRTekk <remi.sulpice@oclock.io>
2024-07-27 21:49:50 +0200

    Stucky Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,7 +3,23 @@ import styles from './style.module.css';
 export default function Index() {
   return (
     <div className={styles.footer}>
-      <p>Rémi Sulpice&copy; 2024</p>
+      <div className={styles.container}>
+        <div className={styles.links}>
+          <div className={styles.navigation}>
+            <h3>Navigation</h3>
+            <a href="#home">Home</a>
+            <a href="#about">Skills</a>
+            <a href="#projects">Projects</a>
+            <a href="#contact">About</a>
+          </div>
+          <div className={styles.socials}>
+            <h3>Socials</h3>
+            <a href="https://www.linkedin.com/in/remi-sulpice/" target="_blank" rel="noreferrer">LinkedIn</a>
+            <a href="https://github.com/0xRTekk" target="_blank" rel="noreferrer">GitHub</a>
+          </div>
+        </div>
+        <h2 className={styles.footerTitle}>2024 &copy;<br />Rémi Sulpice</h2>
+      </div>
     </div>
   )
 }

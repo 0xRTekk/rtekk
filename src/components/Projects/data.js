ab027d1569095df83677d11248530ced28775732
0xRTekk <remi.sulpice@oclock.io>
2024-11-29 22:58:03 +0100

    projects data & animations

---
+++ src/components/Projects/data.js
@@ -0,0 +1,173 @@
+const projectsList = [
+  {
+    title: "Stock Management System",
+    description:
+      "Fullstack Developer for a Stock Management Application for the Agricultural Cooperative CAMPA. CAMPA is an agricultural cooperative bringing together stakeholders across France and Belgium. As a Fullstack Developer, I worked using Agile methodology to draft a requirements document defining their needs. The application ensures traceability of all large agricultural equipment, representing 5% of their revenue. I was responsible for the design and implementation of the frontend, backend, and database components. The application enables tracking of every sale, transfer, and anomaly related to the equipment, while notifying the relevant roles. I developed key features such as authentication, the stock management system for multiple points of sale, the catalog, stock updates and validation processes, as well as the notification system for the responsible managers. Additionally, an automatic reminder system was implemented to prompt managers to declare their inventory in a timely manner.",
+    categories: ["Fullstack"],
+    technologies: ["Next.js", "Express.js", "API", "MySQL", "Docker"],
+    client: "CAMPA",
+    year: "2024",
+    urls: {
+      github: null,
+      live: null,
+    },
+    covers: [
+      {
+        name: "Campa Stock landing page",
+        url: "/images/campa_stock.jpeg",
+      },
+    ],
+  },
+  {
+    title: "Merci Map",
+    description:
+      "CTO for the development of Merci Skateboard's technology subsidiary. The skateboard brand wanted to develop its presence in the digital and innovative sector by creating a mobile application for geolocating routes optimised for alternative forms of transport. The application uses blockchain technology to connect and remunerate its users on a fair and transparent model. I was able to adapt to the technical challenges of the blockchain ecosystem while integrating the brand's visual universe. I was able to build all the technical specifications for the project, from the graphic design to the functional and technical specifications for the mobile application. I defined the roadmap to be followed in order to bring the project to a successful conclusion, right through to the implementation of an MVP and fund-raising, while justifying the appropriate and optimised technological choices.",
+    categories: ["CTO", "Project Management", "MVP", "Web3", "Mobile"],
+    technologies: ["Solidity", "NFT", "React Native", "Ethereum"],
+    client: "Merci Skateboards",
+    year: "2024",
+    urls: {
+      github: null,
+      live: null,
+    },
+    covers: [
+      {
+        name: "",
+        url: "",
+      },
+    ],
+  },
+  {
+    title: "Knight on Base",
+    description:
+      "CTO for a Gaming Blockchain startup. Knight on Base is a heroic-fantasy role-playing game using generative AI and Blockchain technologies. The game uses AI technology to take players through a universe in which they are the heroes and which they build through their choices. Blockchain technology is used to create an engaged community with a high retention rate thanks to a fair and transparent remuneration system. In my role as Tech Lead, I defined the AI and Blockchain technology choices in order to create an MVP for a fund-raising application. I was able to define the technological architecture and the roadmap to bring the project to a successful conclusion. Working in a team consisting of a CEO, a COO and Web3 consultants, I was able to recruit technical talent to strengthen the teams. I was in charge of developing the platform's blockchain using the right technological solutions to ensure a controlled time-to-market and to enable connections with other communities, thereby improving the project's visibility and funding capacity.",
+    categories: ["CTO", "Project Management", "MVP", "Web3", "AI"],
+    technologies: ["Next.js", "Generative AI", "Solidity", "NFT"],
+    client: "Knight on Base",
+    year: "2024",
+    urls: {
+      github: null,
+      live: null,
+    },
+    covers: [
+      {
+        name: "Knight on base landing page",
+        url: "#",
+      },
+    ],
+  },
+  {
+    title: "Online Reservation System",
+    description:
+      "Taking over a client's specifications in order to create an online booking system for social and cultural activities organised for the benefit of employees, their families and retired (former) employees of the 154 companies in the electricity and gas industries. I was able to work in agile mode with a product owner. I was in charge of developing the entire backend and database in accordance with the specifications. I was responsible for developing authentication, the reservation system and updating the availability calendar, the administrator panel and all its possible actions, and setting up the various business management rules.",
+    categories: ["Backend"],
+    technologies: ["React", "Next.js", "Node.js", "PostgreSQL", "Docker"],
+    client: "CMCAS Loire Atlantique Vendée",
+    year: "2024",
+    urls: {
+      github: null,
+      live: null,
+    },
+    covers: [],
+  },
+  {
+    title: "Web programming teacher",
+    description:
+      "I was a web programming teacher for more than 2 years at O'clock, a French web development school. I taught web development to students who wanted to become web developers. I taught them the basics of web development, HTML, CSS, Javascript, PHP, Node.js, Rest API, PostgreSQL and more advanced concepts like Design Patterns, Test Driven Development, Unit Testing, React, Security, DevOps etc. I also taught them how to work in a team, how to use Git, how to use the command line, etc. I was in charge of the new teachers training and onboarding into the pedagogical team. I was also in charge of the students' follow-up and evaluation with a lot of individual support.",
+    categories: ["Teaching"],
+    technologies: ["PHP", "React", "Javascript", "Node.js", "PostgreSQL", "Rest API"],
+    client: "O'clock",
+    year: "2021",
+    urls: {
+      github: null,
+      live: null,
+    },
+    covers: [],
+  },
+  {
+    title: "Why using the refs in React blog post",
+    description:
+      "I wrote a blog post about the use of refs in React. The post explains what refs are, how to use them and why they are useful. The post is aimed at beginners who are just starting to learn React. The post was published on the O'clock blog, a French web development school where I was a teacher for more than 2 years.",
+    categories: ["Technical writing"],
+    technologies: ["React"],
+    client: "O'clock",
+    year: "2022",
+    urls: {
+      github: null,
+      live: null,
+    },
+    covers: [],
+  },
+  {
+    title: "Why using Redux in a React project blog post",
+    description:
+      "I wrote a blog post about the use of Redux in a React project. The post explains what Redux is, how to use it and why it is useful. The post is aimed at beginners who are just starting to learn React. The post was published on the O'clock blog, a French web development school where I was a teacher for more than 2 years.",
+    categories: ["Technical writing"],
+    technologies: ["React", "Redux"],
+    client: "O'clock",
+    year: "2022",
+    urls: {
+      github: null,
+      live: null,
+    },
+    covers: [],
+  },
+  {
+    title: "Prestashop Marketplace modules",
+    description:
+      "I have developed several custom modules for Prestashop Marketplace. These modules allow sellers to manage their products, categories an group of product more efficiently. I also developed a Click & Collect module allowing customers to order online and pick up their order in store during the pandemic. The modules are developed in PHP and use the Prestashop API. I was working at Soledis at the time, a web agency specialized in e-commerce.",
+    categories: ["Fullstack", "e-commerce"],
+    technologies: ["PHP", "Prestashop", "MySQL"],
+    client: "Vetoavenue",
+    year: "2021",
+    urls: {
+      github: null,
+      live: null,
+    },
+    covers: [],
+  },
+  {
+    title: "E-commerce platforms",
+    description:
+      "I have had the opportunity to work on several e-commerce platforms. I have developed custom modules, themes and integrations for various e-commerce platforms such as Prestashop and WooCommerce. I was working at Soledis at the time, a web agency specialized in e-commerce, and the projects was developed using Prestashop, a PHP-based e-commerce platform.",
+    categories: ["Fullstack", "e-commerce"],
+    technologies: ["PHP", "Prestashop", "MySQL"],
+    client: "Vetoavenue",
+    year: "2020",
+    urls: {
+      github: null,
+      live: null,
+    },
+    covers: [],
+  },
+  {
+    title: "Fidelity program",
+    description:
+      "I was in charge of developing a loyalty program for a veterinary specialized e-commerce platform. The module allows customers to accumulate points for each purchase made. These points can be converted into discounts on future purchases. I was working at Soledis at the time, a web agency specialized in e-commerce, and the project was developed using Prestashop, a PHP-based e-commerce platform.",
+    categories: ["Fullstack", "e-commerce"],
+    technologies: ["PHP", "Prestashop", "MySQL"],
+    client: "Vetoavenue",
+    year: "2020",
+    urls: {
+      github: null,
+      live: null,
+    },
+    covers: [],
+  },
+  {
+    title: "Stock management system",
+    description:
+      "I had the opportunity to take over an existing but obsolete application in order to modernise it using new technologies and new security standards. My assignment was to take over an existing MySQL database and remodel it using the latest data security standards and in compliance with the RGPD law. I was able to redesign the entire code base using Symfony for the backend and HTML, CSS, JS and Bootstrap for the frontend. The application is used to monitor the stock of supplies for the various Paris police stations. Automatic alerts are triggered when a certain threshold is reached, enabling managers to place orders quickly and optimise stock management.",
+    categories: ["Fullstack"],
+    technologies: ["PHP", "Symfony", "MySQL", "Docker"],
+    client: "Police headquarters of Paris",
+    year: "2019",
+    urls: {
+      github: null,
+      live: null,
+    },
+    covers: [],
+  },
+];
+
+export default projectsList;
--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,97 +1,6 @@
-import React, { useState, useLayoutEffect, useRef } from "react";
+import React from "react";
 import styles from "./style.module.css";
-import Image from "next/image";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
-const projects = [
-  {
-    title: "Campa Stock",
-    description:
-      "Worked with Socodicor to create a stock management system for agricultures materials and machines.",
-    categories: ["Fullstack"],
-    technologies: ["Next.js", "Express.js", "API", "MySQL", "Docker"],
-    client: "Socodicor",
-    year: "2024",
-    urls: {
-      github: null,
-      live: null,
-    },
-    covers: [
-      {
-        name: "Campa Stock landing page",
-        url: "/images/campa_stock.jpeg",
-      },
-    ],
-  },
-  {
-    title: "Knight on Base",
-    description:
-      "Led the creation of a text based role playing game. The game is built on the Base blockchain and uses generative AI, as a game master, to drive the gamers through their own unique story.",
-    categories: ["Fullstack", "Web3", "AI"],
-    technologies: ["Next.js", "Generative AI", "Solidity", "NFT"],
-    client: "Knight on Base",
-    year: "2024",
-    urls: {
-      github: null,
-      live: null,
-    },
-    covers: [
-      {
-        name: "Knight on base landing page",
-        url: "#",
-      },
-    ],
-  },
-];
-
-// export default function Index() {
-
-//     const [selectedProject, setSelectedProject] = useState(0);
-//     const container = useRef(null);
-//     const imageContainer = useRef(null);
-
-//     useLayoutEffect( () => {
-//         gsap.registerPlugin(ScrollTrigger);
-//         ScrollTrigger.create({
-//             trigger: imageContainer.current,
-//             pin: true,
-//             start: "top-=100px",
-//             end: document.body.offsetHeight - window.innerHeight - 50,
-//         })
-//     }, [])
-
-//     return (
-//         <div ref={container} className={styles.projects}>
-//             <div className={styles.projectDescription}>
-//                 <div ref={imageContainer} className={styles.imageContainer}>
-//                     <Image
-//                         src={`/images/${projects[selectedProject].src}`}
-//                         fill={true}
-//                         alt="project image"
-//                         priority={true}
-//                     />
-//                 </div>
-//                 <div className={styles.column}>
-//                     <p>The flora is characterized by the presence of high elevation wetland, as well as yellow straw, broom sedge, tola de agua and tola amaia.</p>
-//                 </div>
-//                 <div className={styles.column}>
-//                     <p>Some, like the southern viscacha, vicuña and Darwins rhea, are classified as endangered species. Others, such as Andean goose, horned coot, Andean gull, puna tinamou and the three flamingo species inhabiting in Chile (Andean flamingo, Chilean flamingo, and Jamess flamingo) are considered vulnerable.</p>
-//                 </div>
-//             </div>
-
-//             <div className={styles.projectList}>
-//                 {
-//                     projects.map( (project, index) => {
-//                         return <div key={index} onMouseOver={() => {setSelectedProject(index)}} className={styles.projectEl}>
-//                             <h2>{project.title}</h2>
-//                         </div>
-//                     })
-//                 }
-//             </div>
-//         </div>
-//     )
-// }
+import projectsList from "./data";
 
 export default function Index() {
   return (
@@ -111,13 +20,14 @@ export default function Index() {
           <p>Client</p>
           <p>Year</p>
         </div>
-        {projects.map((project, index) => {
+        {projectsList.map((project, index) => {
           return (
-            <div key={index} className={styles.projectItem}>
+            <div key={`project-${index}`} className={styles.projectItem}>
               <p>{project.title}</p>
               <p>{project.categories.join(", ")}</p>
               <p>{project.client}</p>
               <p>{project.year}</p>
+              <div className={styles.projectItemBackground}></div>
             </div>
           );
         })}
